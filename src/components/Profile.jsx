c8fb36549ec34e1b13b22b7a3542322f0171eb83
Ashish Kumar Thakur <105355519+ashishkumarthakur91@users.noreply.github.com>
2025-05-07 19:49:43 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -17,7 +17,7 @@ const Profile = () => {
         },       
         {
             id:3,
-            link:"https://github.com/MrThakurProgrammer",
+            link:"https://github.com/ashishkumarthakur91/",
             icon:<FaGithub />
         },
         {
